9024a64d27a5b28549ff7a73cad9dc95c3b070ce
Karan Sharma <javakaran@live.com>
2013-08-18 21:42:49 +0530

    Arrays in DOTNET

---
+++ Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Arrays
+{
+    public static void Main()
+    {
+        // Single-dimensional array
+        int[] numbers = new int[5];
+        
+        // Multidimensional array
+        string[,] names = new string[5, 4];
+        
+        // Array-of-arrays (jagged array)
+        byte[][] scores = new byte[5][];
+        // Creating the jagged array
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = new byte[i + 3];
+        }
+
+        // Print length of each row
+        for (int i = 0; i < scores.Length; i++)
+        {
+            Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+        }
+
+        Console.ReadLine(); // to halt the display screen
+    }
+}
